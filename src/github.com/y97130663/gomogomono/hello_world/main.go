10b5fe0369cdc41569a8700a4a71de0825ae37ea
y97130663 <y97130663@gmail.com>
2018-07-21 22:58:39 -0500

    added approporiate files

--- src/github.com/y97130663/gomogomono/hello_world/main.go
+++ src/github.com/y97130663/gomogomono/hello_world/main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+type drive interface {
+	carry() string
+}
+
+type vehicle struct{
+	passengers, cargo bool
+}
+
+func (t vehicle) carry() string{
+	if t.cargo && t.passengers {
+		return "Yes it is truck"
+	}
+	return "No it is not a truck"
+}
+
 func main(){
-	fmt.Print("Tesla")
+	fmt.Println("Main function starts here!")
+	t := vehicle {cargo: true, passengers: true}
+	fmt.Println(t.carry())
 }
